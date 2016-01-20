e01bd22d8d6df9c055a502e600fe9622168e0bdc
Teedah Saratoon <teedah.saratoon@gmail.com>
2016-01-20 10:23:20 +0000

    Completed exercise 01-show (typeclasses)

--- src/main/scala/01-show.scala
+++ src/main/scala/01-show.scala
@@ -11,7 +11,25 @@ object ShowExample {
   // TODO:
   //  - You'll need to define some type class instances
   //    to finish the exercises below
-
+  
+  implicit val showForString = new Show[String] {
+    def stringify(value: String): String =
+      "\"" + value.replaceAll("\"", "\\\"") + "\"" 
+  }
+  
+  implicit def showForOption[A](implicit showA: Show[A]) = new Show[Option[A]] {
+    def stringify(value: Option[A]): String =
+      value match {
+        case Some(a) => a.stringify
+        case None    => "<none>"
+      }
+  }
+  
+  implicit def showForList[A](implicit showA: Show[A]) = new Show[List[A]] {
+    def stringify(value: List[A]): String =
+      value.map(showA.stringify).mkString("[", ", ", "]")
+  }
+  
   // Type class interface -----------------------
 
   object Show {
@@ -36,12 +54,23 @@ object ShowExample {
     //  - Print a String to the console
     //    by explicitly summoning an instance of Show
     //    and using its stringify method
+    
+    val stringShow: Show[String] = Show[String]
+    println(stringShow.stringify("This is a string"))
+    
+    // Using the extra syntax we don't need the val (or the println!):
+    "This is another string".print
 
     //  - Print a List of Strings to the console
     //    using the syntax provided in ShowOps
+    
+    List("Hello", "world").print
 
     //  - Print a List of Options of Strings to the console
     //    using any method you like
+    
+    List(Some("Hello"), Some("world"), None).print
+    
   }
 
 }
---
+++ src/main/scala/TestApp.scala
@@ -0,0 +1,7 @@
+
+
+object TestApp extends App {
+  
+  
+  
+}

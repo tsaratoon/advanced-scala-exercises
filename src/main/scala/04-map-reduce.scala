979d2c52c44e72bc12da8ad614dc3deeed4fdf19
Teedah Saratoon <teedah.saratoon@gmail.com>
2016-01-20 12:31:41 +0000

    Working on exercise 04-map-reduce

--- src/main/scala/04-map-reduce.scala
+++ src/main/scala/04-map-reduce.scala
@@ -1,12 +1,32 @@
+import scalaz.Monoid
+import scalaz.std.anyVal._
+import scalaz.syntax.monoid._
+import scala.concurrent.Future
+
 object MapReduceExample {
   // TODO:
+  
   //  - Implement `foldMap`
+	def foldMap[A,B](list: List[A])(f: A => B)(implicit monoid: Monoid[B]): B = {
+		list.foldLeft(mzero[B])(_ |+| f(_))
+	}
+	
   //  - Implement `parallelFoldMap`
+	def parallelFoldMap[A,B](list: List[A])(f: A => B)(implicit monoid: Monoid[B]): Future[B] = {
+	  ???
+	}
+	
   //  - Use each method to sum the lengths of all words in "/usr/share/dict/words" (see below)
   //  - Replace your definition of foldMap with the built-in Scalaz definition
+  
 
   def main(args: Array[String]) = {
-    // println(foldMap(Common.wordList)(_.length))
-    // println(Await.result(parallelFoldMap(Common.wordList)(_.length), 3.seconds))
+
+    val list = List("Hello","world")
+    println(foldMap(list)(s => s.length))
+    
+    println(Common.time(foldMap(Common.wordList)(_.length)))
+    
+//    println(Await.result(parallelFoldMap(Common.wordList)(_.length), 3.seconds))
   }
 }

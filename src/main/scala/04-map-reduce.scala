22697eb2d7f5fd46a99be40a0da226ab75715de2
Teedah Saratoon <teedah.saratoon@gmail.com>
2016-01-20 12:44:12 +0000

    Working on exercise 04-map-reduce

--- src/main/scala/04-map-reduce.scala
+++ src/main/scala/04-map-reduce.scala
@@ -2,6 +2,7 @@ import scalaz.Monoid
 import scalaz.std.anyVal._
 import scalaz.syntax.monoid._
 import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object MapReduceExample {
   // TODO:
@@ -14,7 +15,28 @@ object MapReduceExample {
   //  - Implement `parallelFoldMap`
 	def parallelFoldMap[A,B](list: List[A])(f: A => B)(implicit monoid: Monoid[B]): Future[B] = {
 	  val groups = list.grouped(Common.groupSize(list))
-	  ???
+	  
+	  val groupFutures: Iterator[Future[List[A]]] =
+	    groups.map(group => Future(group))
+	    
+	  val resultFutures: Iterator[Future[B]] = {
+	    groupFutures.map { groupFuture =>
+	      groupFuture.map { group: List[A] =>
+	        foldMap(group)(f)
+	      }
+	    }
+	  }
+	  
+	  // Wait for all futures to be completed before combining results
+	  val resultsFuture: Future[Iterator[B]] =
+	    Future.sequence(resultFutures)
+	    
+	  val resultFuture: Future[B] =
+	    resultsFuture.map { resultsIterator =>
+	      foldMap(resultsIterator.toList)(identity)  // Results are already computed, no need to do anything
+	    }
+	  
+	  resultFuture
 	}
 	
   //  - Use each method to sum the lengths of all words in "/usr/share/dict/words" (see below)
